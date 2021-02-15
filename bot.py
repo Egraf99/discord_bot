5358e9b2ee8ae8330c2abc65ef84830b7291e3ce
Egraf99 <77966173+Egraf99@users.noreply.github.com>
2021-02-15 16:36:43 +0300

    discord bot with function of make embed

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+from random import randint
+from config import settings
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix=settings['prefix'])
 
 
 @bot.event
@@ -9,10 +12,27 @@ async def on_ready():
     print('Bot is ready')
 
 
-@bot.command(pass_context=True)
-async def info(ctx, user: discord.User):
-    emb = discord.Embed(title='Info about {}'.format(user.name))
-    emb.add_field(name='Name', value=user.name)
-    await ctx.channel.send(embed=emb)
+@bot.command(pass_context=True, aliases=['идея'])
+async def __idea(ctx, *args):
+    
+    # choice random color of Embed
+    col = randint(0, 16777215)
 
-bot.run('#your_token')
+    # save send message
+    desc = ''
+    for mes in args:
+        desc += mes + ' '
+
+    # made Embed
+    emb = discord.Embed(title='!идея', description=desc,
+                        colour=col, timestamp=datetime.utcnow())
+    emb.set_footer(text=ctx.message.author.name, icon_url=ctx.message.author.avatar_url)
+
+    # send Embed to the other channel
+    other_channel = bot.get_channel(id=settings['copy_id'])
+    await other_channel.send(embed=emb)
+
+    # delete message
+    await ctx.message.delete()
+
+bot.run(settings['token'])
