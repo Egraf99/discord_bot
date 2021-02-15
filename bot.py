b34f0547586411dd311563b765acb03388f7e4ba
Egraf99 <77966173+Egraf99@users.noreply.github.com>
2021-02-15 16:34:59 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
-from random import randint
-from config import settings
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix=settings['prefix'])
+bot = commands.Bot(command_prefix='/')
 
 
 @bot.event
@@ -12,11 +10,9 @@ async def on_ready():
 
 
 @bot.command(pass_context=True)
-async def идея(ctx, user: discord.User):
-    col = randint(0, 16777215)
-    emb = discord.Embed(title='Info about {}'.format(user.name), colour=col)
+async def info(ctx, user: discord.User):
+    emb = discord.Embed(title='Info about {}'.format(user.name))
     emb.add_field(name='Name', value=user.name)
     await ctx.channel.send(embed=emb)
-    await ctx.message.delete()
 
-bot.run(settings['token'])
+bot.run('#your_token')
